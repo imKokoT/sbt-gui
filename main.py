aa37d45014b0699e303101fcb0dbdc70ecef0588
KokoT <prikolkillercat@gmail.com>
2025-03-15 12:39:37 +0100

    on start checks

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-import tkinter as tk
-import ttkbootstrap as ttk
+from .properties import *
+from logger import logger     # type: ignore
+from runtime_data import rtd  # type: ignore
 
 
 def start_gui():
-    print('test')
+    logger.info(f'plugin "{PLUGIN_NAME}" {VERSION=}')
+    if not RUN_WITH_ANY and rtd['version'] not in SUPPORTS_VERSIONS:
+        logger.error(f'unsupported tool version {rtd['version']}; supports versions: {SUPPORTS_VERSIONS}')
+        exit(1)
+    if RUN_WITH_ANY:
+        logger.warning(f'{RUN_WITH_ANY=}')
 
 
 if __name__ == '__main__':
-    pass
+    print(f'{PLUGIN_NAME=}\n'
+          f'{AUTHOR=}\n'
+          f'{VERSION=}\n'
+          f'{f'{DEBUG=}\n' if DEBUG else ''}'
+          f'{f'{RUN_WITH_ANY=}\n' if RUN_WITH_ANY else ''}')
--- properties.py
+++ properties.py
@@ -1,6 +1,9 @@
 
+PLUGIN_NAME = 'sbt-gui'
+AUTHOR = 'imKokoT'
+
 VERSION = '0.1a'
 DEBUG = True
 
 SUPPORTS_VERSIONS = ('0.10a',)  # supports tool versions 
-RUN_WITH_ANY = True             # run gui with any version if DEBUG is True; NOTE: dev-only!
+RUN_WITH_ANY = True             # run gui with any tool version; NOTE: dev-only!
