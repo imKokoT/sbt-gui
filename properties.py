1614d10ba14992a56aa07f6d3738677ac5fb7106
KokoT <prikolkillercat@gmail.com>
2025-03-15 12:08:31 +0100

    added properties

---
+++ properties.py
@@ -0,0 +1,6 @@
+
+VERSION = '0.1a'
+DEBUG = True
+
+SUPPORTS_VERSIONS = ('0.10a',)  # supports tool versions 
+RUN_WITH_ANY = True             # run gui with any version if DEBUG is True; NOTE: dev-only!
