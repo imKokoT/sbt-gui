76a8487ebb963b33079bbf4fec4693df2b44d7d9
KokoT <prikolkillercat@gmail.com>
2025-03-16 11:27:10 +0100

    - backup button - added padding to frame and elements

--- menu_gui.py
+++ menu_gui.py
@@ -15,11 +15,13 @@ class MenuGUI(ttk.Window):
 
         # --- widgets ---
         self.main_frame = ttk.Frame(self)
-        self.main_frame.grid(row=0, column=0)
+        self.main_frame.grid(row=0, column=0, pady=10)
 
         self.selectSchema_cb = ttk.Combobox(self.main_frame, values=['test1', 'test2', 'test3'])
         self.selectSchema_cb.current(0)
         self.label1 = ttk.Label(self.main_frame, text='Schema: ', justify='right')
+        self.backup_but = ttk.Button(self.main_frame, text='Backup')
 
         self.label1.grid(row=0, column=0)
         self.selectSchema_cb.grid(row=0, column=1, )
+        self.backup_but.grid(row=1, columnspan=2, sticky='ew', pady=4)
