5b53e27d27d68a49fefe2cfde0429ad879bfedd0
KokoT <prikolkillercat@gmail.com>
2025-03-18 15:10:18 +0100

    added scroll text to display logs

--- backup_gui.py
+++ backup_gui.py
@@ -1,6 +1,7 @@
 import sys
 import threading
 import tkinter as tk
+from tkinter import scrolledtext
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from miscellaneous.events import pushEvent
@@ -14,17 +15,21 @@ class BackupGUI(ttk.Toplevel):
         from . import styles
         
         self.title(f'Creating backup of "{schemaName}"')
-        self.geometry('300x160')
+        self.geometry('600x300')
         self.attributes("-toolwindow", False)
-        self.resizable(False, False)
+        # self.resizable(False, False)
         self.protocol("WM_DELETE_WINDOW", lambda: (pushEvent('cancel-process'), self.after(0, self.destroy))[-1])
         
         # --- widgets ---
-        self.progress = ttk.Progressbar(self, orient=HORIZONTAL, mode='determinate', length=280, 
+        self.progress_bar = ttk.Progressbar(self, orient=HORIZONTAL, mode='determinate', length=580, maximum=1,
                                         style='SBT.Horizontal.TProgressbar')
-        self.progress['value'] = 50
-        self.progress.pack(pady=10, padx=10)
+        self.progress_bar['value'] = 0.5
+        self.logs_st = scrolledtext.ScrolledText(self, wrap=tk.WORD, width=590, height=200)
+
+        self.progress_bar.pack(pady=10, padx=10, fill='x')
+        self.logs_st.pack(pady=15, padx=5, expand=True)
         
+        # --- other ---
         self.transient(parent)  # Attach to main window
         self.grab_set()  # Lock interaction with the main window
 
